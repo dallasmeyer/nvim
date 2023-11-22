062acc5d40190cc2e7546d4fcdc22abc879064d9
dallasmeyer <80178092+dallasmeyer@users.noreply.github.com>
2023-11-22 02:38:58 -0800

    removed comment shortcut <leader>/, and added leetcode info comment

--- lua/plugins/mini-comment.lua
+++ lua/plugins/mini-comment.lua
@@ -1,18 +1,18 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- disable the keymap to grep files
-      { "<leader>/", false },
-    },
-  },
-
-  {
-    "echasnovski/mini.comment",
-    opts = {
-      mappings = {
-        comment_line = "<leader>/",
-      },
-    },
-  },
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   keys = {
+  --     -- disable the keymap to grep files
+  --     { "<leader>/", false },
+  --   },
+  -- },
+  --
+  -- {
+  --   "echasnovski/mini.comment",
+  --   opts = {
+  --     mappings = {
+  --       comment_line = "<leader>/",
+  --     },
+  --   },
+  -- },
 }
