d9dc95ca6d9f3aa7fe762373c6d9b753536a02ea
Dallas Meyer <dallasmeyer@Dals-Air.attlocal.net>
2023-12-19 01:11:57 -0800

    changed neovide refresh rate to 60, changed colorscheme

--- init.lua
+++ init.lua
@@ -3,12 +3,13 @@ require("config.lazy")
 
 if vim.g.neovide then
   vim.opt.guifont = { "JetBrainsMono Nerd Font", ":h11" }
-  vim.g.neovide_refresh_rate = 165
+  vim.g.neovide_refresh_rate = 60
 end
 
 -- Lua initialization file
 vim.g.nightflyTransparent = true
-vim.cmd([[colorscheme nightfly]])
+-- vim.cmd([[colorscheme nightfly]])
+vim.cmd([[colorscheme dracula]])
 
 -- removes notify error for transparent background
 require("notify").setup({
